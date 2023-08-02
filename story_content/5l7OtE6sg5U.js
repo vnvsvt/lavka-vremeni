6283be35ebabf07c51420c450c71585b5e9d09fa
vnvsvt <140352755+vnvsvt@users.noreply.github.com>
2023-08-03 01:32:40 +0300

    второй шаг и оптимизация картинок

---
+++ story_content/5l7OtE6sg5U.js
@@ -0,0 +1,1 @@
+window.globalProvideSvgData('svg-5l7OtE6sg5U', "<svg width=\"1920\" height=\"990\" viewBox=\"0 0 1920 990\" fill=\"none\" xmlns=\"http://www.w3.org/2000/svg\" id=\"svg-5l7OtE6sg5U\"><g pointer-events=\"visiblePainted\" data-accepts=\"events\"><rect opacity=\"0.6\" width=\"1920\" height=\"1080\" fill=\"black\" /></g></svg>");
